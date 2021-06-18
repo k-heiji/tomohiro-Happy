984fb2dc97be29320a5cf6ddda47ece2c1eb06c3
k-heiji <86001272+k-heiji@users.noreply.github.com>
2021-06-19 04:47:18 +0900

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -15,7 +15,7 @@ const titles = document.getElementById('titles');
 //const differ=tomorrow.getTime()-now.getTime();//明日まであと何ミリ秒か
 
 // 来年の1月1日00:00:00のDateオブジェクトを取得
-const birthdayTime = new Date(`2021/06/20 00:00:00`);
+const birthdayTime = new Date(`2021/06/19 04:50:00`);
 
 // 来年の値をDOMに追加
 // year.innerText = birthdayTime;
